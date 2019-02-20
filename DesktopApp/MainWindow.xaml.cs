9717541dbb719f519e80dc537d6a1087c49faa2c
Torstein Winjum <twinjum@hotmail.com>
2019-02-20 16:01:58 +0100

    ymse wpf

--- DesktopApp/MainWindow.xaml.cs
+++ DesktopApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,35 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DesktopApp
+namespace HotelBooking
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// test 2
+    /// test kommentar
+    /// 
     public partial class MainWindow : Window
     {
+        ObservableCollection<TaskDummy> TaskDummyList { get; set; }
+
         public MainWindow()
         {
+            DataContext = this;
+            TaskDummyList = new ObservableCollection<TaskDummy> {
+                new TaskDummy {Type = "Room Service", RoomNumber = 101, Description = "More pizza!", TimeAdded = new DateTime(2019,1,20,14,13,0) },
+                new TaskDummy {Type = "Room Service", RoomNumber = 102, Description = "More beer!", TimeAdded = new DateTime(2019,1,20,14,12,0) },
+                new TaskDummy {Type = "Maintainance", RoomNumber = 101, Description = "Muh fan not werking", TimeAdded = new DateTime(2019,1,20,12,13,0) }
+            };
             InitializeComponent();
         }
+    }
+
+    class TaskDummy
+    {
+        public string Type { get; set; }
+        public int RoomNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeAdded { get; set; }
     }
 }
