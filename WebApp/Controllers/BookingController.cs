ad6710df8a5e00122a339d59980311d887435e12
Torstein Winjum <twinjum@hotmail.com>
2019-02-22 15:05:11 +0100

    Flyttet db til ekstern dll, la til taskscontroller, renamet tasks i db til ServiceTask, fordi Task allerede er en ting vi trenger

--- WebApp/Controllers/BookingController.cs
+++ WebApp/Controllers/BookingController.cs
@@ -4,39 +4,38 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DesktopApp;
 
 namespace WebApp.Controllers
 {
-    public class BookingController : Controller
-    {
-        DesktopAppConfig dac = new DesktopAppConfig();
-        DbSet<Room> room;
-        DbSet<Booking> booking;
-        DbSet<Customer> customer;
+    //public class BookingController : Controller
+    //{
+    //    DesktopAppConfig dac = new DesktopAppConfig();
+    //    DbSet<Room> room;
+    //    DbSet<Booking> booking;
+    //    DbSet<Customer> customer;
 
-        room = dac.Room;
-        booking = dac.Booking;
-        customer = dac.Customer;
+    //    room = dac.Room;
+    //    booking = dac.Booking;
+    //    customer = dac.Customer;
 
-        room.Load();
-        booking.Load();
-        customer.Load();
+    //    room.Load();
+    //    booking.Load();
+    //    customer.Load();
 
         
 
-        public bool CheckUserNameExists(string username)
-        {
-            var isDuplicate = dac.Customer.Any(r => r.Username == dac.);
+    //    public bool CheckUserNameExists(string username)
+    //    {
+    //        var isDuplicate = dac.Customer.Any(r => r.Username == dac.);
 
-            if (isDuplicate)
-            {
-                // Inform user that the lot_number already exists
-                return true;
-            }
+    //        if (isDuplicate)
+    //        {
+    //            // Inform user that the lot_number already exists
+    //            return true;
+    //        }
 
-            Console.WriteLine("does not exist. yay");
+    //        Console.WriteLine("does not exist. yay");
 
-        }
-    }
+    //    }
+    //}
 }
