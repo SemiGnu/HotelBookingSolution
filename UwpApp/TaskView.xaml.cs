0529bfe9c7e7763231a37109bd0458a397fea737
Torstein Winjum <twinjum@hotmail.com>
2019-02-25 15:20:50 +0100

    tok vekk den tabben fra desktop, litt skring i uwp'en

--- UwpApp/TaskView.xaml.cs
+++ UwpApp/TaskView.xaml.cs
@@ -101,33 +101,39 @@ namespace UwpApp
                 task.Wait();
                 rooms = JsonConvert.DeserializeObject<List<Room>>(response);
             }
+            
             Int32.TryParse(TaskEntryRoomNumber.Text, out int roomNo);
             int roomId = rooms.Where(room => room.RoomNumber == roomNo).First<Room>().RoomId;
 
-            ServiceTask st = new ServiceTask
+            if (roomId != 0)
             {
-                TypeOfService = CurrentServiceType,
-                Status = "Pending",
-                TimeIssued = DateTime.Now,
-                TimeCompleted = null,
-                RoomId = roomId,
-                Description = TaskEntryDescription.Text
-            };
-
-            var newServiceTask = JsonConvert.SerializeObject(st);
-            var buffer = System.Text.Encoding.UTF8.GetBytes(newServiceTask);
-            var byteContent = new ByteArrayContent(buffer);
-            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            using (var client = new HttpClient())
-            {
-                Task task = Task.Run(async () =>
+                ServiceTask st = new ServiceTask
                 {
-                    await client.PostAsync(new Uri("http://localhost:52285/api/ServiceTasks"), byteContent);
-                });
-                task.Wait();
+                    TypeOfService = CurrentServiceType,
+                    Status = "Pending",
+                    TimeIssued = DateTime.Now,
+                    TimeCompleted = null,
+                    RoomId = roomId,
+                    Description = TaskEntryDescription.Text
+                };
+
+                var newServiceTask = JsonConvert.SerializeObject(st);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(newServiceTask);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                using (var client = new HttpClient())
+                {
+                    Task task = Task.Run(async () =>
+                    {
+                        await client.PostAsync(new Uri("http://localhost:52285/api/ServiceTasks"), byteContent);
+                    });
+                    task.Wait();
+                }
+                UpdateServiceTaskList(); 
             }
-            UpdateServiceTaskList();
+            TaskEntryDescription.Text = "";
+            TaskEntryRoomNumber.Text = "";
         }
 
         private void CompleteTaskButton_Click(object sender, RoutedEventArgs e)
