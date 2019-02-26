1948d44c769719c0b7a0dab1400f6fe5c1875beb
Torstein Winjum <twinjum@hotmail.com>
2019-02-26 14:33:16 +0100

    reservation request handler

---
+++ WebApp/Models/RequestHandler.cs
@@ -0,0 +1,45 @@
+﻿using DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class RequestHandler
+    {
+        public static Booking HandleRequest(ReservationRequest rr)
+        {
+            Booking returnBooking = null;
+
+
+            if (rr.isValid())
+            {
+                dat154_19_2Entities db = new dat154_19_2Entities();
+
+                var bestRoom = db.Room.Where(r => r.NumberOfBeds >= rr.NumberOfBeds)
+                    .Join(db.Booking,
+                    r => r.RoomId,
+                    b => b.RoomId,
+                    (r, b) => new { R = r, B = b })
+                    .Where(rb => rb.B.CheckOutDate <= rr.CheckInDate || rb.B.CheckInDate >= rr.CheckOutDate)
+                    .OrderBy(rb => rb.R.NumberOfBeds)
+                    .Select(rb => rb.R)
+                    .FirstOrDefault();
+
+                if (bestRoom != null)
+                {
+                    returnBooking = new Booking
+                    {
+                        CustomerUsername = rr.Username,
+                        CheckInDate = rr.CheckInDate,
+                        CheckOutDate = rr.CheckOutDate,
+                        RoomId = bestRoom.RoomId
+                    };
+                }
+                
+            }
+            return returnBooking;
+        }
+    }
+}
---
+++ WebApp/Models/ReservationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class ReservationRequest
+    {
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int NumberOfBeds { get; set; }
+        public string Username { get; set; }
+
+        public bool isValid() {
+            return CheckInDate != null && CheckOutDate != null && NumberOfBeds > 0 && Username != null;
+        }
+    }
+}
