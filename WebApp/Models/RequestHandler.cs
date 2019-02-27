45924099009deeb2b9538c7bab986debdb858738
Torstein Winjum <twinjum@hotmail.com>
2019-02-27 14:53:27 +0100

    liten fiks på RequestHandler i web/models

--- WebApp/Models/RequestHandler.cs
+++ WebApp/Models/RequestHandler.cs
@@ -17,8 +17,17 @@ namespace WebApp.Models
             {
                 dat154_19_2Entities db = new dat154_19_2Entities();
 
-                var bookedRoomIds = db.Booking.Where(bo => !(bo.CheckOutDate.CompareTo(rr.CheckInDate) < 0 || bo.CheckInDate.CompareTo(rr.CheckOutDate) > 0)).Select(bo => bo.RoomId);
-                var bestRoom = db.Room.Where(rm => rm.NumberOfBeds >= rr.NumberOfBeds).Where(rm => !bookedRoomIds.Contains(rm.RoomId)).OrderBy(rm => rm.NumberOfBeds).FirstOrDefault();
+                // finner id på rom booket i perioden
+                var bookedRoomIds = db.Booking
+                    .Where(bo => !(bo.CheckOutDate.CompareTo(rr.CheckInDate) < 0 || bo.CheckInDate.CompareTo(rr.CheckOutDate) > 0))
+                    .Select(bo => bo.RoomId);
+
+                // finner rom med nok senger, som ikke er i forrige query, henter ut den med færrest rom av ledige
+                var bestRoom = db.Room
+                    .Where(rm => rm.NumberOfBeds >= rr.NumberOfBeds)
+                    .Where(rm => !bookedRoomIds.Contains(rm.RoomId))
+                    .OrderBy(rm => rm.NumberOfBeds)
+                    .FirstOrDefault();
 
                 if (bestRoom != null)
                 {
